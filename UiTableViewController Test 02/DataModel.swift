653678cd9f0fd9c6b176d00afb15c08b58b37225
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-06-18 14:56:50 +0900

    first

---
+++ UiTableViewController Test 02/DataModel.swift
@@ -0,0 +1,12 @@
+//
+//  DataModel.swift
+//  UiTableViewController Test 02
+//
+//  Created by D7702_10 on 2018. 6. 18..
+//  Copyright © 2018년 lse. All rights reserved.
+//
+
+import Foundation
+class DataModel {
+    
+}
